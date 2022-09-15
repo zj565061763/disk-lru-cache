9d967f7f41d1b31046b841f292ab6f5e9eccc66a
565061763 <565061763@qq.com>
2022-09-15 15:13:27 +0800

    first commit

---
+++ app/src/main/java/com/sd/demo/disk_lru_cache/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sd.demo.disk_lru_cache
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
