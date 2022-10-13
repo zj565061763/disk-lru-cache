ab2666c8e75f16635f7f7339e58b684dfcf86cfc
565061763 <565061763@qq.com>
2022-10-14 00:53:56 +0800

    FDiskLruCache

--- lib/src/main/java/com/sd/lib/dlcache/FDiskLruCache.kt
+++ lib/src/main/java/com/sd/lib/dlcache/FDiskLruCache.kt
@@ -4,33 +4,35 @@ import java.io.File
 
 class FDiskLruCache(directory: File) : IDiskLruCache {
 
-    private val _cache by lazy { InternalDiskLruCache.dir(directory) }
+    private val _directory = directory
+    private val cache: IDiskLruCache
+        get() = InternalDiskLruCache.dir(_directory)
 
     override fun setMaxSize(maxSize: Long) {
-        _cache.setMaxSize(maxSize)
+        cache.setMaxSize(maxSize)
     }
 
     override fun put(key: String, file: File?): Boolean {
-        return _cache.put(key, file)
+        return cache.put(key, file)
     }
 
     override fun get(key: String): File? {
-        return _cache.get(key)
+        return cache.get(key)
     }
 
     override fun remove(key: String): Boolean {
-        return _cache.remove(key)
+        return cache.remove(key)
     }
 
     override fun size(): Long {
-        return _cache.size()
+        return cache.size()
     }
 
     override fun edit(key: String, block: (editFile: File) -> Boolean): Boolean {
-        return _cache.edit(key, block)
+        return cache.edit(key, block)
     }
 
     override fun close() {
-        _cache.close()
+        cache.close()
     }
 }
