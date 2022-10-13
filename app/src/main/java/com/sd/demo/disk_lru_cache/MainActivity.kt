cb2db4f88a33d7aa5a6611af0f4f287f06718df6
565061763 <565061763@qq.com>
2022-10-13 20:56:19 +0800

    demo

--- app/src/main/java/com/sd/demo/disk_lru_cache/MainActivity.kt
+++ app/src/main/java/com/sd/demo/disk_lru_cache/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.sd.demo.disk_lru_cache
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.sd.demo.disk_lru_cache.databinding.ActivityMainBinding
+import com.sd.lib.dlcache.FDiskLruCache
+import com.sd.lib.io.FFileUtils
+import com.sd.lib.io.FIOUtils
+import com.sd.lib.io.dir.ext.FDirTemp
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val _binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val _diskLruCache by lazy {
+        FDiskLruCache.dir(FFileUtils.getCacheDir("app_cache"))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(_binding.root)
+
+        _binding.btnPut.setOnClickListener {
+            putCache()
+        }
+        _binding.btnGet.setOnClickListener {
+            getCache()
+        }
+    }
+
+    private fun putCache() {
+        val tempFile = FDirTemp.newFile("txt")
+        FIOUtils.writeText(UUID.randomUUID().toString(), tempFile)
+
+        _diskLruCache.put("key", tempFile)
+    }
+
+    private fun getCache() {
+        val file = _diskLruCache.get("key") ?: return
+        Log.i(TAG, FIOUtils.readText(file) ?: "")
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
