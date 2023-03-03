777c0e413672d1fedf0c8263d19bfd53afcc694c
zj565061763 <565061763@qq.com>
2023-03-03 18:52:56 +0800

    1.1.2

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "disk-lru-cache"
-val libVersionName = "1.1.1"
+val libVersionName = "1.1.2"
 
 android {
     namespace ="com.sd.lib.dlcache"
