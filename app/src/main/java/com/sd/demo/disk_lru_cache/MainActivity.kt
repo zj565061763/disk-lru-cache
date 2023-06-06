218a36bb7680b563909d6ac331d830cd838e0b04
zj565061763 <565061763@qq.com>
2023-06-06 19:08:24 +0800

    demo

--- app/src/main/java/com/sd/demo/disk_lru_cache/MainActivity.kt
+++ app/src/main/java/com/sd/demo/disk_lru_cache/MainActivity.kt
@@ -3,40 +3,29 @@ package com.sd.demo.disk_lru_cache
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.sd.demo.disk_lru_cache.ui.theme.AppTheme
+import com.sd.demo.disk_lru_cache.databinding.ActivityMainBinding
 import com.sd.lib.dlcache.FDiskLruCache
 import java.io.File
 import java.util.UUID
 
 class MainActivity : ComponentActivity() {
+    private val _binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val _cache by lazy { FDiskLruCache(externalCacheDir!!) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AppTheme {
-                Content(
-                    onClickPut = {
-                        putCache()
-                    },
-                    onClickGet = {
-                        getCache()
-                    },
-                    onClickRemove = {
-                        removeCache()
-                    },
-                )
-            }
+        setContentView(_binding.root)
+
+        _binding.btnPut.setOnClickListener {
+            putCache()
+        }
+
+        _binding.btnGet.setOnClickListener {
+            getCache()
+        }
+
+        _binding.btnRemove.setOnClickListener {
+            removeCache()
         }
     }
 
@@ -60,37 +49,6 @@ class MainActivity : ComponentActivity() {
     private fun removeCache() {
         val remove = _cache.remove("key")
         logMsg { "removeCache $remove" }
-    }
-}
-
-@Composable
-private fun Content(
-    onClickPut: () -> Unit,
-    onClickGet: () -> Unit,
-    onClickRemove: () -> Unit,
-) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(5.dp)
-    ) {
-        Button(
-            onClick = onClickPut
-        ) {
-            Text(text = "put")
-        }
-
-        Button(
-            onClick = onClickGet
-        ) {
-            Text(text = "get")
-        }
-
-        Button(
-            onClick = onClickRemove
-        ) {
-            Text(text = "remove")
-        }
     }
 }
 
