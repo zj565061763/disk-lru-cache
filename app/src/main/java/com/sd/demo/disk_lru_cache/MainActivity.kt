d41d25782e7149610e9f6e7af794c7e6116da71f
zjun <565061763@qq.com>
2024-01-30 21:08:39 +0800

    demo

--- app/src/main/java/com/sd/demo/disk_lru_cache/MainActivity.kt
+++ app/src/main/java/com/sd/demo/disk_lru_cache/MainActivity.kt
@@ -5,13 +5,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import com.sd.demo.disk_lru_cache.databinding.ActivityMainBinding
 import com.sd.lib.dlcache.FDiskLruCache
+import com.sd.lib.dlcache.IDiskLruCache
 import java.io.File
 import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     private val _binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    private val _cache by lazy { FDiskLruCache.create(externalCacheDir!!) }
+    private var _cache: IDiskLruCache? = null
+
+    private val cache: IDiskLruCache
+        get() = _cache ?: FDiskLruCache.create(externalCacheDir!!).also { _cache = it }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,20 +40,25 @@ class MainActivity : ComponentActivity() {
             writeText(fileContent)
         }
 
-        val put = _cache.put("key", tempFile)
+        val put = cache.put("key", tempFile)
         tempFile.delete()
 
         logMsg { "putCache $fileContent $put" }
     }
 
     private fun getCache() {
-        val readContent = _cache.get("key")?.readText()
+        val readContent = cache.get("key")?.readText()
         logMsg { "getCache $readContent" }
     }
 
     private fun removeCache() {
-        val remove = _cache.remove("key")
+        val remove = cache.remove("key")
         logMsg { "removeCache $remove" }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        _cache = null
     }
 }
 
