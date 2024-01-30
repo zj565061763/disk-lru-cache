69e2aabf2910957ea5583399baa266ee38246baf
zjun <565061763@qq.com>
2024-01-30 17:52:04 +0800

    update

--- lib/src/main/java/com/sd/lib/dlcache/core/Util.java
+++ lib/src/main/java/com/sd/lib/dlcache/core/Util.java
@@ -24,7 +24,7 @@ import java.io.StringWriter;
 import java.nio.charset.Charset;
 
 /** Junk drawer of utility methods. */
-class Util {
+final class Util {
   static final Charset US_ASCII = Charset.forName("US-ASCII");
   static final Charset UTF_8 = Charset.forName("UTF-8");
 
